0d977877afc602b4ba21f0c635fda2f9d0feb19b
unknown <2020583117@qq.com>
2024-10-07 14:40:50 +0800

    feat: init next-theme

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
+import { useTheme } from 'next-themes'
+
 import { Button } from '@/components/ui/button'
 
 const Site = () => {
+  const { setTheme } = useTheme()
+
   return (
     <div>
-      <Button>Click me</Button>
+      <Button onClick={() => setTheme('system')}>System</Button>
+      <Button onClick={() => setTheme('light')}>light</Button>
+      <Button onClick={() => setTheme('dark')}>dark</Button>
     </div>
   )
 }
