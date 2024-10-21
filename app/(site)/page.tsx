ee27b2ea6683359b16e822e6d947f36c8dcbd87e
unknown <2020583117@qq.com>
2024-10-21 09:26:46 +0800

    style: 更新格式化相关

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes"
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button"
 
 const Site = () => {
   const { setTheme } = useTheme()
 
   return (
     <div>
-      <Button onClick={() => setTheme('system')}>System</Button>
-      <Button onClick={() => setTheme('light')}>light</Button>
-      <Button onClick={() => setTheme('dark')}>dark</Button>
+      <Button onClick={() => setTheme("system")}>System</Button>
+      <Button onClick={() => setTheme("light")}>light</Button>
+      <Button onClick={() => setTheme("dark")}>dark</Button>
     </div>
   )
 }
